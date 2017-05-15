ea06ad04652d51bc3201fc047b33a83b6491d490
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-15 16:29:30 +0300

    create simple server with express

--- app/app.js
+++ app/app.js
@@ -1,11 +1,23 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-
-var myServer = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.write('<h1>Roux Meetups</h1>');
-  res.end();
+app.get('/', function (req, res) {
+  res.send('<h1>Roux Academy Meetups</h1>');
 });
 
-myServer.listen(3000);
-console.log('Go to http://localhost:3000 on your browser');
+var server = app.listen(3000, function () {
+  console.log('Listening on port 3000')
+});
+
+
+// var http = require('http');
+
+
+// var myServer = http.createServer(function (req, res) {
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   res.write('<h1>Roux Meetups</h1>');
+//   res.end();
+// });
+
+// myServer.listen(3000);
+// console.log('Go to http://localhost:3000 on your browser');
