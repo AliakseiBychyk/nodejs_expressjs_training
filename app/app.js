9b40be6bd1f2a3eeac478bc9309fdf6880c26d78
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-15 16:21:38 +0300

    add html tags

--- app/app.js
+++ app/app.js
@@ -2,9 +2,10 @@ var http = require('http');
 
 
 var myServer = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.write('Roux Meetups');
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write('<h1>Roux Meetups</h1>');
   res.end();
 });
 
 myServer.listen(3000);
+console.log('Go to http://localhost:3000 on your browser');
