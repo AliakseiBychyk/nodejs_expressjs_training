082937025abfd68f4f095f134c7b3af19aada858
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-16 11:33:01 +0300

    install a templating engine

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res) {
-  res.send(`
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <h1>Welcome to Roux Meetups! voila!</h1>
-    <h2> le Beaujolais nouveau est arrive!</h2>
-    <img src="/images/misc/background.jpg" 
-    alt="background" style="height: 300px" />
-    <p>Roux Academy Meetups put together artists from all walks of life</p>
-    <script src="/reload/reload.js"></script>
-  `);
+
+  res.render('index');
+
+  // res.send(`
+  //   <link rel="stylesheet" type="text/css" href="css/style.css" />
+  //   <h1>Welcome to Roux Meetups! voila!</h1>
+  //   <h2> le Beaujolais nouveau est arrive!</h2>
+  //   <img src="/images/misc/background.jpg" 
+  //   alt="background" style="height: 300px" />
+  //   <p>Roux Academy Meetups put together artists from all walks of life</p>
+  //   <script src="/reload/reload.js"></script>
+  // `);
 });
 
 module.exports = router;
