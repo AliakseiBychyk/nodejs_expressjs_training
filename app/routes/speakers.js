3c8d8121577a0fccbf626f6c11194807583e1ce6
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2017-05-16 10:34:49 +0300

    creating a public folder

--- app/routes/speakers.js
+++ app/routes/speakers.js
@@ -8,12 +8,15 @@ router.get('/speakers', function (req, res) {
     info += `
     <li>
       <h2>${item.name}</h2>
+      <img src="/images/speakers/${item.shortname}_tn.jpg" 
+      alt="speaker" /> 
       <p>${item.summary}</p>
     </li>
     `;
   })
   res.send(`
-    <h1>Roux Academy Meetups</h1>
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    <h1>Roux Academy Meetups</h1> 
     ${info}
   `);
 });
@@ -22,8 +25,11 @@ router.get('/speakers/:speakerId', function (req, res) {
   var dataFile = req.app.get('appData');
   var speaker = dataFile.speakers[req.params.speakerId]; 
   res.send(`
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
     <h1>${speaker.title}</h1>
     <h2>with ${speaker.name}</h2>
+    <img src="/images/speakers/${speaker.shortname}_tn.jpg" 
+      alt="speaker" />
     <p>${speaker.summary}</p>
   `);
 });
